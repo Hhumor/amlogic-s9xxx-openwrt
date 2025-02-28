b6b88a38bbd9145337bf228b4f8bcb030e6f0b65
Hhumor <hhumormail@gmail.com>
2025-02-28 13:26:20 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -34,3 +34,28 @@ git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 #
 # ------------------------------- Other ends -------------------------------
 
+PKG_PATH="/builder/openwrt/package/"
+
+#修复TailScale配置文件冲突
+TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+if [ -f "$TS_FILE" ]; then
+        sed -i '/\/files/d' $TS_FILE
+
+        cd $PKG_PATH && echo "tailscale has been fixed!"
+fi
+
+#修复Coremark编译失败
+CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
+if [ -f "$CM_FILE" ]; then
+        sed -i 's/mkdir/mkdir -p/g' $CM_FILE
+
+        cd $PKG_PATH && echo "coremark has been fixed!"
+fi
+
+#修复libffi编译失败
+LF_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/libffi/Makefile")
+if [ -f "$LF_FILE" ]; then
+        sed -i '/\/autoreconf/d' $LF_FILE
+
+        cd $PKG_PATH && echo "libffi has been fixed!"
+fi
