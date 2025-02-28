ca191168ab0c46253c31fe8ac45c27d92e72890a
Hhumor <hhumormail@gmail.com>
2025-02-28 13:25:51 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -12,28 +12,3 @@
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
-PKG_PATH="/builder/openwrt/package/"
-
-#修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-        sed -i '/\/files/d' $TS_FILE
-
-        cd $PKG_PATH && echo "tailscale has been fixed!"
-fi
-
-#修复Coremark编译失败
-CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
-if [ -f "$CM_FILE" ]; then
-        sed -i 's/mkdir/mkdir -p/g' $CM_FILE
-
-        cd $PKG_PATH && echo "coremark has been fixed!"
-fi
-
-#修复libffi编译失败
-LF_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/libffi/Makefile")
-if [ -f "$LF_FILE" ]; then
-        sed -i '/\/autoreconf/d' $LF_FILE
-
-        cd $PKG_PATH && echo "libffi has been fixed!"
-fi
